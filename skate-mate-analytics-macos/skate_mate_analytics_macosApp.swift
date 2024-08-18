31755cc1d2d4d3e90ef2af319df7cd3d40443c81
Gabe Susman <gabe.susman@gmail.com>
2024-08-18 02:58:50 -0600

    Initial Commit

---
+++ skate-mate-analytics-macos/skate_mate_analytics_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  skate_mate_analytics_macosApp.swift
+//  skate-mate-analytics-macos
+//
+//  Created by Max Gabriel Susman on 8/18/24.
+//
+
+import SwiftUI
+
+@main
+struct skate_mate_analytics_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
